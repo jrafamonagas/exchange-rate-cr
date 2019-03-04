72c0d62485ef2a6f3c6a6a1ffcff7e97fd3cbcc0
Kevin Wolf <hi@kevinwolf.me>
2019-03-03 21:49:38 -0600

    refactor: move app.tsx code to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { unstable_createRoot as createRoot } from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
+import { ThemeProvider } from 'emotion-theming'
 
-import App from './app'
+import theme from './config/theme'
+import Container from './components/container'
+import Content from './components/content'
+import Footer from './components/footer'
+import Loader from './components/loader'
+import ExchangeRate from './components/exchange-rate'
+
+function App(): JSX.Element {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Content>
+          <Suspense maxDuration={250} fallback={<Loader />}>
+            <ExchangeRate />
+          </Suspense>
+        </Content>
+        <Footer />
+      </Container>
+    </ThemeProvider>
+  )
+}
 
 library.add(faExchangeAlt)
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-
-root.render(<App />)
+createRoot(document.getElementById('root')).render(<App />)
